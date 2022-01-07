1c19a39c061cc171c69036c3fd383576a4cecfe0
Konrad Czardybon <konradcz2001@gmail.com>
2022-01-07 20:02:07 +0100

    Initial commit

---
+++ src/main/java/com/github/konradcz2001/medicalvisits/MedicalVisitsApplication.java
@@ -0,0 +1,13 @@
+package com.github.konradcz2001.medicalvisits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalVisitsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalVisitsApplication.class, args);
+    }
+
+}
